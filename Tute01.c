e2873bb67dbd3cfbd78ecdf98df4ae2d97f29d4c
Gihan sudeepa <86099252+GIHAA@users.noreply.github.com>
2022-02-09 09:48:19 +0000

    I have completed the tutorial.

--- Tute01.c
+++ Tute01.c
@@ -10,10 +10,10 @@ int main() {
   int mark2;
   float average;
 
-  printf("Enter marks for subject 1 : ");
+  printf("Enter marks for subject 1: ");
   scanf("%d", &mark1 );
 
-  printf("Enter marks for subject 2 : ");
+  printf("Enter marks for subject 2: ");
   scanf("%d", &mark2 );
 
   average = (mark1 + mark2)/2.0;
